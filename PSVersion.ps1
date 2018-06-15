545103c8959abd96ccbaa0243cbcc30e555f4e1d
Sami Hassan <37025175+smkhassan@users.noreply.github.com>
2018-06-15 13:25:29 +0200

    Add files via upload

---
+++ PSVersion.ps1
@@ -0,0 +1,15 @@
+﻿$prefix = $Global:globalVariables.prefix
+$ftpBaseResultFolder = "PS Reports"
+$ftpResultFolder = "$ftpBaseResultFolder/$($Global:globalVariables.prefix)"
+
+
+$ipaddress = $(ipconfig | where {$_ -match 'IPv4.+\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' } | out-null; $Matches[1])
+$PSShell = $PSVersionTable.PsVersion.Major 
+"IP Address of the server"+" " +$ipaddress +"  " + "Version of PS is" +"  "+ $PSShell | out-file 'c:\temp\version.txt'
+
+$localFile = 'c:\temp\version.txt'
+New-FTPDirectory $ftpBaseResultFolder
+New-FTPDirectory $ftpResultFolder
+
+Send-File "$($ftpResultFolder)/$($Global:globalVariables.prefix).txt" $localFile
+LogMessage "PS Version report uploaded."
